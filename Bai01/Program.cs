c0be2e82ff8edab578f8fbd41774f06001c8e945
TSang <sangdang513@gmail.com>
2025-10-19 22:24:30 +0700

    bai1 and 2

---
+++ Bai01/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            int month, year;
+            while (true)
+            {
+                Console.Write("Nhập tháng: ");
+                month = int.Parse(Console.ReadLine());
+                Console.Write("Nhập năm: ");
+                year = int.Parse(Console.ReadLine());
+                if (month < 1 || month > 12 || year < 0)
+                {
+                    Console.WriteLine("Tháng hoặc năm không hợp lệ, vui lòng nhập lại!");
+                }
+                else
+                    break;
+            }
+            // Lấy ngày đầu tiên của tháng
+            DateTime first_day_of_a_month = new DateTime(year, month, 1);
+            // Lấy thứ của ngày đầu tiên trong tháng
+            var first_day_of_week_of_a_month = first_day_of_a_month.DayOfWeek;
+
+            // Lấy số ngày trong tháng
+            int days_in_month = DateTime.DaysInMonth(year, month);
+            // In lịch
+            Console.WriteLine("\nMonth:{0}/{1}",month.ToString().PadLeft(2,'0'),year);
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("| Sun | Mon | Tue | Wed | Thu | Fri | Sat |");
+            Console.WriteLine("-------------------------------------------");
+            for (int i = 0; i < (int)first_day_of_week_of_a_month; i++)
+            {
+                Console.Write("|     ");
+            }
+            for (int day = 1; day <= days_in_month; day++)
+            {
+                Console.Write("|  {0} ", day.ToString().PadRight(2));
+                if (DayOfWeek.Saturday == (new DateTime(year, month, day).DayOfWeek))
+                {
+                    Console.WriteLine("|");
+                    Console.WriteLine("-------------------------------------------");
+                }
+            }
+            // In các ô trống còn lại trong tuần cuối cùng của tháng
+            int last_day_of_week_of_a_month = (int)(new DateTime(year, month, days_in_month).DayOfWeek);
+            
+            for (int i = last_day_of_week_of_a_month + 1; i <= 6; i++)
+            {
+                Console.Write("|     ");
+                if (i == 6)
+                {
+                    Console.WriteLine("|");
+                    Console.WriteLine("-------------------------------------------");
+                }
+            }
+        }
+    }
+}
---
+++ Bai02/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Bai02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.OutputEncoding = Encoding.UTF8;
+                Console.Write("Nhập đường dẫn thư mục: ");
+                string path = Console.ReadLine();
+                if (Directory.Exists(path))
+                {
+                    DirectoryInfo dirInfo = new DirectoryInfo(path);
+                    Console.WriteLine("Thư mục hiện có là: " + dirInfo.FullName);
+                    var allFile = dirInfo.GetFiles(); // Lấy tất cả các file trong thư mục                   
+                    var allDir = dirInfo.GetDirectories(); // Lấy tất cả các thư mục con trong thư mục
+                    int i = 0;
+                    int j = 0;
+                    while (i < allFile.Length || j < allDir.Length)
+                    {
+                        if (i >= allFile.Length)
+                        {
+                            Console.WriteLine($"{allDir[j].LastWriteTime:dd/MM/yyyy hh:mm tt}             <DIR>   {allDir[j].Name}");
+                            j++;
+                            continue;
+                        }
+                        if (j >= allDir.Length)
+                        {
+                            Console.WriteLine($"{allFile[i].LastWriteTime:dd/MM/yyyy hh:mm tt}   {allFile[i].Length,15:N0}   {allFile[i].Name}");
+                            i++;
+                            continue;
+                        }
+                        if (String.Compare(allFile[i].Name, allDir[j].Name, StringComparison.OrdinalIgnoreCase) <= 0)
+                        {
+                            Console.WriteLine($"{allFile[i].LastWriteTime:dd/MM/yyyy hh:mm tt}   {allFile[i].Length,15:N0}   {allFile[i].Name}");
+                            i++;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{allDir[j].LastWriteTime:dd/MM/yyyy hh:mm tt}             <DIR>   {allDir[j].Name}");
+                            j++;
+                        }
+                    }
+
+                    // Tổng file
+                    Console.WriteLine($"{allFile.Length} file(s) : {allFile.Sum(f => f.Length):N0} bytes");
+
+                    // tổng thư mục                  
+                    Console.WriteLine($"{allDir.Length} directorie(s) ");
+                }
+                else
+                    Console.WriteLine("Đường dẫn thư mục không tồn tại.");
+            }
+        }
+
+    
+    }
+}
